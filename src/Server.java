e28c134fc5fa380fcac5072040caef248d2f58d1
Avin Chadee <achadee15@gmail.com>
2013-11-18 00:00:05 +1100

    finished basic SC

--- src/Server.java
+++ src/Server.java
@@ -1,20 +1,33 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		if (args.length != 1){
-			printError("Invalid amount of arguments specified, you need one!");
+		if (args.length != 2){
+			printError("Invalid amount of arguments specified, you need two!");
 			System.out.println("Port Number");
+			System.out.println("Max Threads");
 		}
 		
-		//define the max amount of users
-		ExecutorService executor = Executors.newFixedThreadPool(5);
-		
-		
+		 // Register service on port 1234
+        ServerSocket s = new ServerSocket(Integer.parseInt(args[0]));
+        ExecutorService pool = Executors.newFixedThreadPool(Integer.parseInt(args[1]));
+
+        // listen for new connections, create new request handler to process request
+        try {
+                while (true) {
+                        pool.execute(new ThreadInstance(s.accept()));
+                }
+        } catch (IOException ioe) {
+                pool.shutdown();
+        }
+
+        s.close();
 		
 	}
 
--- src/ThreadInstance.java
+++ src/ThreadInstance.java
@@ -1,18 +1,41 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 
 public class ThreadInstance implements Runnable {
 
-	public ThreadInstance(Socket clientSocket){
-		
-	}
+    private Socket s;
+    private Dictionary dictionary;
 	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-		
-		
+	public ThreadInstance(Socket clientSocket){
+        this.s = clientSocket;
+        this.dictionary = new Dictionary();
 	}
+
+    public void run()
+    {
+            try {
+                    InputStream sin = s.getInputStream();
+                    DataInputStream dis = new DataInputStream(sin);
+                    String word = new String(dis.readUTF());
+
+                    String meaning = new String(dictionary.getMeaning(word));
+
+                    OutputStream sout = s.getOutputStream();
+                    DataOutputStream dos = new DataOutputStream(sout);
+                    dos.writeUTF(meaning);
+
+                    dos.close();
+                    sout.close();
+                    s.close();
+            }
+            catch (IOException ioe) {
+                    ioe.printStackTrace();
+            }
+    }
 
 }
